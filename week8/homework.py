0fa99b1d6fa4698ef752570c9ebf82cb93374dbf
Aiseen <aysen.sagynbekov@mail.com>
2022-07-06 15:06:41 +0600

    newww

---
+++ week8/homework.py
@@ -0,0 +1,29 @@
+# class Makers:
+#     name = 'Aisen'
+#     student_quantity = 20
+
+#     def __init__(self,language,kpi):
+#         self.language = language
+#         self.kpi = kpi
+    
+#     def new_student(self,new):
+#         # self.new = self.student_quantity =+ 1
+#         self.new = new
+#         self.new = self.student_quantity + 1
+#         return f'всего - {self.new} студентов с вами.'
+
+#     def siu(self):
+#         return f'ваш язык обучения - {self.language},а ваше имя - {self.name}'
+
+#     def mark(self):
+#         if self.kpi < 10:
+#             print('ваша оценка 3')
+#         else :
+#             print('ваша оценка 5')
+
+
+
+# a = Makers('aisen',30)
+# print(a.new_student(1))
+# print(a.siu())
+# a.mark()
